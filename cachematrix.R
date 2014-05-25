11d539d246c0a4c980a0a3b32a36e53484048463
Ivan Molto <ivanmolto@users.noreply.github.com>
2014-05-25 16:23:03 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        
+        ## set the value
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setmatrix <- function(solve) m <<- solve
-        getmatrix <- function() m
-        
-        ## set the value
         ## get the value
+        get <- function() x
         ## set the value of solve
+        setmatrix <- function(solve) m <<- solve
         ## get the value of solve
-        
+        getmatrix <- function() m
         list (set = set, get = get, 
               setmatrix = setmatrix, 
               getmatrix = getmatrix)
